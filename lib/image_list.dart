fd3f235b73063e450f86bc6759b369bd504e8adb
LeeHyo-Jeong <hyojung040102@naver.com>
2024-06-28 16:47:01 +0900

    implement gallery

--- lib/image_list.dart
+++ lib/image_list.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:photo_manager/photo_manager.dart';
+import 'dart:typed_data';
 
 class ImageWidget extends StatefulWidget {
   const ImageWidget({super.key});
@@ -10,21 +11,71 @@ class ImageWidget extends StatefulWidget {
 }
 
 class _ImageWidgetState extends State<ImageWidget> {
-
-  XFile? image; // 갤러리에서 image picker를 통해 받아온 이미지 파일
+  List<AssetEntity> _mediaList = [];
 
   @override
   void initState(){
     super.initState();
+    _loadMedia();
+  }
+
+  Future<void> _loadMedia() async{
+    // 갤러리 접근 권한 요청
+    final PermissionState ps = await PhotoManager.requestPermissionExtend();
+    print('PermissionState: ${ps.isAuth}, ${ps.hasAccess}');
+    // 권한 허용 되었다면 갤러리로부터 이미지 fetch
+    if(ps.isAuth){
+      List<AssetPathEntity> albums = await PhotoManager.getAssetPathList(
+        // 이미지 앨범만 가져온다
+          type: RequestType.image
+      );
+
+      List<AssetEntity> media = await albums[0].getAssetListPaged(page: 0, size: 100);
+
+      // _mediaList 업데이트 하고 UI 다시 빌드
+      setState(() {
+        _mediaList = media;
+      });
+    }
+    // 권한이 허용되지 않은 경우 설정 화면을 연다
+    else{
+      await PhotoManager.openSetting();
+    }
   }
 
   @override
   void dispose(){
-    super.initState();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _mediaList.isEmpty
+          ? Center(child:CircularProgressIndicator(color: Color(0xff98e0ff)))
+          : GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, mainAxisSpacing: 5, crossAxisSpacing: 5,
+        ),
+        itemCount: _mediaList.length,
+        itemBuilder: (context, index){
+          return FutureBuilder<Uint8List?>(
+            future: _mediaList[index].thumbnailData,
+            builder: (context, snapshot){
+              if(snapshot.connectionState == ConnectionState.done && snapshot.data != null) {
+                return Image.memory(
+                  snapshot.data!,
+                  fit: BoxFit.cover,
+                );
+              }
+              else{
+                return Center(
+                    child: CircularProgressIndicator());
+              }
+            },
+          );
+        },
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:madcamp_project01/contact_list.dart';
+import 'package:madcamp_project01/image_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,59 +29,12 @@ class MyApp extends StatelessWidget {
           body: TabBarView(
             children: [
               ContactsWidget(), // 연락처 위젯 사용
-              Center(child: Text('gallery')),
+              ImageWidget(),
               Center(child: Text('Content for Tab 3')),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
