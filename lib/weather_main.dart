c034cbfd3758ab4e9f3009ef7bf14f3221d66e48
LeeHyo-Jeong <hyojung040102@naver.com>
2024-07-01 17:38:02 +0900

    add functions to check weather at user's current location

--- lib/weather_main.dart
+++ lib/weather_main.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:timer_builder/timer_builder.dart';
 import 'package:intl/intl.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Weather {
   final String description;
@@ -12,14 +13,17 @@ class Weather {
   final int sunrise;
   final int sunset;
   final iconUrl;
+  final cityName;
 
-  Weather(
-      {required this.description,
-      required this.temperature,
-      required this.humidity,
-      required this.sunrise,
-      required this.sunset,
-      required this.iconUrl});
+  Weather({
+    required this.description,
+    required this.temperature,
+    required this.humidity,
+    required this.sunrise,
+    required this.sunset,
+    required this.iconUrl,
+    required this.cityName,
+  });
 
   factory Weather.fromJson(Map<String, dynamic> json) {
     return Weather(
@@ -30,6 +34,7 @@ class Weather {
       sunset: json['sys']['sunset'],
       iconUrl:
           'https://openweathermap.org/img/wn/${json['weather'][0]['icon']}@2x.png',
+      cityName: json['name'],
     );
   }
 }
@@ -41,6 +46,35 @@ class WeatherWidget extends StatefulWidget {
 
 class _WeatherWidgetState extends State<WeatherWidget> {
 
+  late Future<Weather> _weatherFuture;
+
+  Future<Position> getPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      // 설정 열어서 권한 받을 수 있도록 수정하기
+      return Future.error('Location services are disalbed.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissoins are permanently denied.');
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
   Future<Weather> getWeather() async {
     final String? apiKey = await dotenv.env['openWeatherApiKey'];
     final String? apiBaseUrl = await dotenv.env['openWeatherApiBaseUrl'];
@@ -50,7 +84,10 @@ class _WeatherWidgetState extends State<WeatherWidget> {
     Weather weather;
 
     try {
-      Uri url = Uri.parse('$apiBaseUrl?appid=$apiKey');
+      final position = await getPosition();
+
+      Uri url = Uri.parse(
+          '$apiBaseUrl?lat=${position.latitude}&lon=${position.longitude}&appid=${apiKey}');
       response = await http.get(url);
 
       if (response.statusCode == 200) {
@@ -67,14 +104,16 @@ class _WeatherWidgetState extends State<WeatherWidget> {
     }
   }
 
-  void _initdata() async{
+  void _initdata() async {
     WidgetsFlutterBinding.ensureInitialized();
     await dotenv.load(fileName: "assets/config/.env");
+    _weatherFuture = getWeather();
   }
 
   @override
   void initState() {
     _initdata();
+    _weatherFuture = getWeather();
     super.initState();
   }
 
@@ -83,7 +122,7 @@ class _WeatherWidgetState extends State<WeatherWidget> {
     return new DateFormat("h:mm a").format(now);
   }
 
-  String formatTimeStamp(int timestamp){
+  String formatTimeStamp(int timestamp) {
     var date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
     return DateFormat("h:mm a").format(date);
   }
@@ -92,6 +131,7 @@ class _WeatherWidgetState extends State<WeatherWidget> {
   Widget build(BuildContext context) {
     var date = DateTime.now();
     final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Color(0xff98e0ff),
       extendBodyBehindAppBar: true,
@@ -110,92 +150,108 @@ class _WeatherWidgetState extends State<WeatherWidget> {
                     SizedBox(
                       height: 50,
                     ),
-                    Row(
-                      children: [
-                        SizedBox(width: 10),
-                        Text('Daejeon',
-                            style: TextStyle(
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            )),
-                      ],
-                    ),
-                    Row(children: [
-                      SizedBox(width: 10),
-                      TimerBuilder.periodic(Duration(minutes: 1),
-                          builder: (context) {
-                        return Text('${getSystemTime()}',
-                            style:
-                                TextStyle(fontSize: 16.0, color: Colors.white));
-                      }),
-                      Text(
-                        DateFormat(' - EEEE, ').format(date),
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        DateFormat('d MMM, yyy').format(date),
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ]),
-                    SizedBox(
-                      height: size.height * 0.15,
-                    ),
+                    FutureBuilder<Weather>(
+                        future: _weatherFuture,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                                  ConnectionState.waiting ||
+                              (snapshot.hasError &&
+                                  snapshot.error.toString() ==
+                                      "Invalid argument(s): No host specified in URI null?appid=null")) {
+                            return Center(child: CircularProgressIndicator());
+                          } else if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          } else if (snapshot.hasData) {
+                            Weather weather = snapshot.data!;
+                            return Column(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(children: [
+                                      SizedBox(width: 10),
+                                      Text('${weather.cityName}',
+                                          style: TextStyle(
+                                            fontSize: 40,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                          )),
+                                      IconButton(
+                                        icon: Icon(Icons.location_searching),
+                                        color: Colors.white,
+                                        onPressed: () {
+                                          setState((){
+                                            _weatherFuture = getWeather();
+                                          });
+                                        },
+                                      ),
+                                    ]),
+                                    Row(children: [
+                                      SizedBox(width: 10),
+                                      TimerBuilder.periodic(
+                                          Duration(minutes: 1),
+                                          builder: (context) {
+                                        return Text('${getSystemTime()}',
+                                            style: TextStyle(
+                                                fontSize: 16.0,
+                                                color: Colors.white));
+                                      }),
+                                      Text(
+                                        DateFormat(' - EEEE, ').format(date),
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      Text(
+                                        DateFormat('d MMM, yyy').format(date),
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ]),
+                                  ],
+                                ),
+                                SizedBox(height: size.height * 0.15),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                        '${(weather.temperature - 273.15).toStringAsFixed(0)}°',
+                                        style: TextStyle(
+                                          fontSize: 70,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        )),
+                                    Image.network(weather.iconUrl),
+                                    Text('${weather.description}',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                        )),
+                                    SizedBox(height: 10),
+                                    Text('humidity: ${weather.humidity}%',
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.white)),
+                                    Text(
+                                        'sunrise: ${formatTimeStamp(weather.sunrise)}',
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.white)),
+                                    Text(
+                                        'sunset: ${formatTimeStamp(weather.sunset)}',
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.white))
+                                  ],
+                                )
+                              ],
+                            );
+                          } else {
+                            return Text('No data available');
+                          }
+                        }),
                   ],
                 ),
-                FutureBuilder<Weather>(
-                    future: getWeather(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting || (snapshot.hasError && snapshot.error.toString() == "Invalid argument(s): No host specified in URI null?appid=null")) {
-                        return CircularProgressIndicator();
-                      } else if (snapshot.hasError) { // 왜 에러가 발생하지..?
-                        return Text('Error: ${snapshot.error}');
-                      } else if (snapshot.hasData) {
-                        Weather weather = snapshot.data!;
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                                '${(weather.temperature - 273.15).toStringAsFixed(0)}°',
-                                style: TextStyle(
-                                  fontSize: 70,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                            Image.network(weather.iconUrl),
-                            Text('${weather.description}',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                )),
-                            SizedBox(height: 10),
-                            Text('humidity: ${weather.humidity}%',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white
-                            )),
-                            Text('sunrise: ${formatTimeStamp(weather.sunrise)}',
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.white
-                                )),
-                            Text('sunset: ${formatTimeStamp(weather.sunset)}',
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.white
-                                ))
-                          ],
-                        );
-                      } else {
-                        return Text('No data available');
-                      }
-                    }),
               ],
             ),
           ),
