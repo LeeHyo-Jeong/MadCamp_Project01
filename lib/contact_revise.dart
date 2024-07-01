ee87d65ed85617ad4803ffaa5887239677fce35a
WonjungKim <wjhj16@kaist.ac.kr>
2024-07-01 15:43:47 +0900

    add image picker

--- lib/contact_revise.dart
+++ lib/contact_revise.dart
@@ -75,51 +75,53 @@ class _ContactReviseState extends State<ContactRevise> {
     avatarRadius = screenWidth * 0.1;
 
     return Scaffold(
-          appBar: AppBar(
-            // backgroundColor: Color(0xfff7f2f9),
-            title: Text(
-              "Revise Contract",
+      appBar: AppBar(
+        // backgroundColor: Color(0xfff7f2f9),
+        title: Text(
+          "Revise Contract",
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context, false);
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              reviseContact(givencontact, firstname, lastname, email,
+                  phone); // 연락처가 수정된다.
+              Navigator.pop(context, true);
+            },
+            child: Text(
+              "save",
               style: TextStyle(
-                fontSize: 25,
+                fontSize: 17,
                 color: Colors.black,
               ),
             ),
-            centerTitle: true,
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context, false);
-              },
-            ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  reviseContact(givencontact, firstname, lastname, email,
-                      phone); // 연락처가 수정된다.
-                  Navigator.pop(context, true);
-                },
-                child: Text(
-                  "save",
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ],
           ),
-          body: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(height: screenHeight * 0.07,),
-                  givencontact.avatar != null && givencontact.avatar!.isNotEmpty
-                      ? CircleAvatar(
-                    backgroundImage: MemoryImage(givencontact.avatar!),
-                    radius: avatarRadius * 2.5, // 반지름 설정
-                  )
-                      : CircleAvatar(
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: screenHeight * 0.07,
+              ),
+              givencontact.avatar != null && givencontact.avatar!.isNotEmpty
+                  ? CircleAvatar(
+                      backgroundImage: MemoryImage(givencontact.avatar!),
+                      radius: avatarRadius * 2.5, // 반지름 설정
+                    )
+                  : CircleAvatar(
                       backgroundColor: Color(0xff98e0ff), // 색깔 변경하기?
                       // 배경색 설정 (원형 아바타를 만들 때 중요)
                       radius: avatarRadius * 2.5,
@@ -129,69 +131,71 @@ class _ContactReviseState extends State<ContactRevise> {
                         color: Colors.white, // 아이콘 색상 설정
                         size: avatarRadius * 3.5, // 아이콘 크기 설정
                       )),
-                  SizedBox(height: screenHeight * 0.07,),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    elevation: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          _buildTextField(
-                            label: "First name",
-                            controller: firstnameController,
-                            onChanged: (value) {
-                              setState(() {
-                                firstname = value;
-                              });
-                            },
-                            icon: Icons.person,
-                          ),
-                          SizedBox(height: 20),
-                          _buildTextField(
-                            label: "Last name",
-                            controller: lastnameController,
-                            onChanged: (value) {
-                              setState(() {
-                                lastname = value;
-                              });
-                            },
-                            icon: Icons.person,
-                          ),
-                          SizedBox(height: 20),
-                          _buildTextField(
-                            label: "Phone",
-                            controller: phoneController,
-                            onChanged: (value) {
-                              setState(() {
-                                phone = value;
-                              });
-                            },
-                            icon: Icons.phone,
-                          ),
-                          SizedBox(height: 20),
-                          _buildTextField(
-                            label: "Email",
-                            controller: emailController,
-                            onChanged: (value) {
-                              setState(() {
-                                email = value;
-                              });
-                            },
-                            icon: Icons.email,
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+              SizedBox(
+                height: screenHeight * 0.07,
               ),
-            ),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                elevation: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildTextField(
+                        label: "First name",
+                        controller: firstnameController,
+                        onChanged: (value) {
+                          setState(() {
+                            firstname = value;
+                          });
+                        },
+                        icon: Icons.person,
+                      ),
+                      SizedBox(height: 20),
+                      _buildTextField(
+                        label: "Last name",
+                        controller: lastnameController,
+                        onChanged: (value) {
+                          setState(() {
+                            lastname = value;
+                          });
+                        },
+                        icon: Icons.person,
+                      ),
+                      SizedBox(height: 20),
+                      _buildTextField(
+                        label: "Phone",
+                        controller: phoneController,
+                        onChanged: (value) {
+                          setState(() {
+                            phone = value;
+                          });
+                        },
+                        icon: Icons.phone,
+                      ),
+                      SizedBox(height: 20),
+                      _buildTextField(
+                        label: "Email",
+                        controller: emailController,
+                        onChanged: (value) {
+                          setState(() {
+                            email = value;
+                          });
+                        },
+                        icon: Icons.email,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
-        );
+        ),
+      ),
+    );
   }
 
   Widget _buildTextField({
